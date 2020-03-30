5ae9712e67318a86f55d8e422797ecb06ae2791e
YukaLangbuana <langbuana.yuka@hotmail.com>
2020-03-30 16:21:20 +0700

    server code and gamescreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,197 @@
-import { Component } from 'react'
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Alert
+} from "react-native";
+import Pusher from 'pusher-js/react-native';
+
+const pusher_app_key = '76592d5da6fa9c9cb523';
+const pusher_app_cluster = 'ap1';
+const base_url = 'http://ac385c16.ngrok.io';
 
 export default class LoginScreen extends Component {
-    render(){
-        return(null)
+
+    state = {
+        username: "",
+        enteredGame: false
+    };
+
+    constructor(props) {
+        super(props);
+        this.pusher = null;
+        this.myChannel = null;
+    }
+
+    enterGame = async () => {
+        const username = this.state.username;
+
+        if (username) {
+            this.setState({
+                enteredGame: true // show loading text
+            });
+
+            this.pusher = new Pusher(pusher_app_key, {
+                authEndpoint: `${base_url}/pusher/auth`,
+                cluster: pusher_app_cluster,
+                auth: {
+                    params: { username: username }
+                },
+                encrypted: true
+            });
+
+            this.myChannel = this.pusher.subscribe(`private-user-${username}`);
+            this.myChannel.bind("pusher:subscription_error", status => {
+                Alert.alert(
+                    "Error",
+                    "Subscription error occurred. Please restart the app"
+                );
+            });
+
+            this.myChannel.bind("pusher:subscription_succeeded", () => {
+                this.myChannel.bind("opponent-found", data => {
+                    let opponent = username == data.player_one ? data.player_two : data.player_one;
+
+                    const playerOneObjects = {
+                        plank: "plankOne",
+                        wall: "leftWall",
+                        plankColor: "green"
+                    };
+
+                    const playerTwoObjects = {
+                        plank: "plankTwo",
+                        wall: "rightWall",
+                        plankColor: "blue"
+                    };
+
+                    const isPlayerOne = username == data.player_one ? true : false;
+
+                    const myObjects = isPlayerOne ? playerOneObjects : playerTwoObjects;
+                    const opponentObjects = isPlayerOne
+                        ? playerTwoObjects
+                        : playerOneObjects;
+
+                    const myPlank = myObjects.plank;
+                    const myPlankColor = myObjects.plankColor;
+                    const opponentPlank = opponentObjects.plank;
+                    const opponentPlankColor = opponentObjects.plankColor;
+
+                    const myWall = myObjects.wall;
+                    const opponentWall = opponentObjects.wall;
+
+                    Alert.alert("Opponent found!", `Your plank color is ${myPlankColor}`);
+
+                    this.opponentChannel = this.pusher.subscribe(
+                        `private-user-${opponent}`
+                    );
+                    this.opponentChannel.bind("pusher:subscription_error", data => {
+                        console.log("Error subscribing to opponent's channel: ", data);
+                    });
+
+                    this.opponentChannel.bind("pusher:subscription_succeeded", () => {
+
+                        this.props.navigation.navigate("Game", {
+                            pusher: this.pusher,
+                            username: username,
+                            myChannel: this.myChannel,
+                            opponentChannel: this.opponentChannel,
+
+                            opponent: opponent,
+                            isPlayerOne: isPlayerOne,
+                            myPlank: myPlank,
+                            opponentPlank: opponentPlank,
+                            myPlankColor: myPlankColor,
+                            opponentPlankColor: opponentPlankColor,
+
+                            myWall: myWall,
+                            opponentWall: opponentWall
+                        });
+                    });
+
+                    this.setState({
+                        username: "",
+                        enteredGame: false
+                    });
+                });
+            });
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.wrapper}>
+
+                <View style={styles.container}>
+                    <View style={styles.main}>
+                        <View>
+                            <Text style={styles.label}>Enter your username</Text>
+                            <TextInput
+                                style={styles.textInput}
+                                onChangeText={username => this.setState({ username })}
+                                value={this.state.username}
+                            />
+                        </View>
+
+                        {
+                            !this.state.enteredGame &&
+                            <TouchableOpacity onPress={this.enterGame}>
+                                <View style={styles.button}>
+                                    <Text style={styles.buttonText}>Login</Text>
+                                </View>
+                            </TouchableOpacity>
+                        }
+
+                        {
+                            this.state.enteredGame && (
+                                <Text style={styles.loadingText}>Loading...</Text>
+                            )
+                        }
+                    </View>
+                </View>
+            </View>
+        );
     }
 }
+
+
+
+const styles = {
+    wrapper: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+        backgroundColor: "#FFF"
+    },
+    fieldContainer: {
+        marginTop: 20
+    },
+    label: {
+        fontSize: 16
+    },
+    textInput: {
+        height: 40,
+        marginTop: 5,
+        marginBottom: 10,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        backgroundColor: "#eaeaea",
+        padding: 5
+    },
+    button: {
+        alignSelf: "center",
+        marginTop: 10
+    },
+    buttonText: {
+        fontSize: 18,
+        color: "#05a5d1"
+    },
+    loadingText: {
+        alignSelf: "center"
+    }
+};
